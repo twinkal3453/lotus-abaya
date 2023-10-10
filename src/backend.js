0df11e3734ddf37089c98622718f058bac39cd59
twinkal3453 <twinkalkrraj@gmail.com>
2023-10-10 10:06:29 +0530

    main: Changed the env configration in the backend.js file.

--- src/backend.js
+++ src/backend.js
@@ -1,4 +1,4 @@
-export const API = process.env.REACT_APP_BACKEND;
-export const PATH = process.env.REACT_APP_FILE_PATH;
+export const API = process.env.REACT_APP_BACKEND_DEV;
+export const PATH = process.env.REACT_APP_FILE_PATH_DEV;
 export const PUBLISH = process.env.REACT_APP_STRIPE;
 export const RECAPTCHA = process.env.REACT_APP_RECAPTCHA;
